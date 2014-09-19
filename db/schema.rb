b991eefe081d300e83db08acfceba1cf2377db32
Eslam-Saeed <eslamsaeed022@gmail.com>
2014-09-19 14:58:50 +0200

    My new branch after t3dilo:)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140918141912) do
+ActiveRecord::Schema.define(version: 20140918143711) do
 
   create_table "customers", force: true do |t|
     t.string   "first_name"
