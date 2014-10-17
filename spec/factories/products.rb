fc4530f262e2dff5cdec30c7a842aeca6a16b3ae
MoamenHossam <moamen.hossam93@gmail.com>
2014-10-17 10:15:20 +0200

    Commentin FactoryGirl

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,9 +1,9 @@
-FactroyGirls.define do 
-	factory :product do
-		product_name "moamen"
-		product_price "50"
-		product_count "5"
+# FactroyGirls.define do 
+# 	factory :product do
+# 		product_name "moamen"
+# 		product_price "50"
+# 		product_count "5"
 		
-	end
+# 	end
 	
-end
+# end
