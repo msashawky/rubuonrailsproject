84fb5e657c52db8b285e9f419d51c8ad298c8ad4
MoamenHossam <moamen.hossam93@gmail.com>
2014-12-08 21:24:23 +0200

    Modify Product controller to suit Category change

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,6 +21,7 @@ before_action :authenticate_user!
 
   # GET /products/new
   def new
+    @category =ProductCategory.all
     @product = Product.new
   end
 
@@ -63,7 +64,7 @@ before_action :authenticate_user!
   def destroy
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
+      format.html { redirect_to products_url, notice: 'Product was successfully deleted.' }
       format.json { head :no_content }
     end
   end
@@ -77,7 +78,7 @@ before_action :authenticate_user!
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
 
-      params.require(:product).permit(:product_name, :product_price, :product_count, :product_description,:photo)
+      params.require(:product).permit(:product_name, :product_price, :product_count, :product_description,:photo).merge(:product_categories_id => params[:product_categories_id][:id])
     end
 
   def add_product
