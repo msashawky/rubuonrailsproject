849add74cfe3fb7fbf2fddf32200b8122f01a8e6
MoamenHossam <moamen.hossam93@gmail.com>
2014-12-12 23:32:48 +0200

    issue_90_Redirect well

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20141209023331) do
-=======
 ActiveRecord::Schema.define(version: 20141211183029) do
->>>>>>> master
 
   create_table "accountants", force: true do |t|
     t.datetime "created_at"
