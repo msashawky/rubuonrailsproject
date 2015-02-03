aa7390ca73f0d16b7803936a8606706e2122468e
Moamen Abdelwahed <moomen.abdelwahed@gmail.com>
2015-02-03 15:50:56 +0200

    Enable assets precompile for production environment

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -19,6 +18,9 @@ module MarketPlace
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.compile = true
+    config.assets.digest = true
     config.serve_static_assets = true
     config.assets.paths << Rails.root.join('vendor', 'assets')
   end
